9f34645f6d7a0ced386a59285cdf4ec926ef05ea
Yendry <yen.badilla@gmail.com>
2021-10-19 23:03:41 -0600

    Complete selection function, genetic function, delete element from playr

--- src/genetic.rkt
+++ src/genetic.rkt
@@ -3,12 +3,11 @@
 #|Construye a un jugardor ingresando sus características en una lista.
 id-player: número del jugador
 id-team: equipo al que pertenece
-position: posición en el mapa
 desplazamiento: velocidad con la que corre en la cancha
 fuerza: fuerza con la que le da a la bola
 |#
 (define (create-player id-player id-team)
-  (list id-team id-player 4 (list-ref (list 0 1 2 3 4 5 6 7 8 9 10) (random 10)) (list-ref (list 0 1 2 3 4 5 6 7 8 9 10) (random 10))))
+  (list id-team id-player (list-ref (list 0 1 2 3 4 5 6 7 8 9 10) (random 10)) (list-ref (list 0 1 2 3 4 5 6 7 8 9 10) (random 10))))
 
 ; Busca el equipo especificado
 (define (get-team id-team game)
@@ -27,38 +26,25 @@ fuerza: fuerza con la que le da a la bola
          (get-player (cdr team) player-id))))
 
 ; Obtienen las características de un jugador especificado
-(define (get-id-player player)
-  (cadr player))
 
 (define (get-id-team player)
   (car player))
 
-(define (get-position player)
-  (caddr player))
+(define (get-id-player player)
+  (cadr player))
 
 (define (get-movement player)
-  (cadddr player))
+  (caddr player))
 
 (define (get-force player)
-  (cadr(cdddr player)))
+  (cadddr player))
 
 (define (get-fitness player)
-  (caddr(cdddr player)))
-
-; Realiza la seleccion de los jugadores con mayor fitness
-(define (selection player)
-  (cond ((> (get-fitness player) 3)
-         (list (get-id-team player) (get-id-player player)(get-position player)(get-movement player)(get-force player)(get-fitness player)))
-        
-  ))
-
-; Cruce: realiza el cruce de las caracteristica entre dos jugadores
-(define (cruce player)
-  (list (get-id-team player)(get-id-player player)(get-position player)(/ (+ (get-movement player)(get-movement player)) 2) (/ (+ (get-force player)(get-force player)) 2)))
+  (cadr(cdddr player)))
 
 ; Funcion de fitness:  
 (define (fitness player)
-  (list (get-id-team player) (get-id-player player) (get-position player) (get-movement player) (get-force player) (exact-round(/ (+ (* (get-movement player)(/ 75 100)) (* (get-force player)(/ 25 100))) 2))))
+  (list (get-id-team player) (get-id-player player)(get-movement player) (get-force player) (exact-round(/ (+ (* (get-movement player)(/ 75 100)) (* (get-force player)(/ 25 100))) 2))))
 
 ; Crea la lista con los jugadores
 (define (create-team total-players id-team)
@@ -71,25 +57,49 @@ fuerza: fuerza con la que le da a la bola
 (define (game team1 team2)
   (list team1 "/" team2))
 
+; Elimina un elemento de la lista
+(define (delete-player list player)
+  (cond ((null? list)
+         list)
+        ((equal? (car list) player)
+         (delete-player (cdr list) player)
+         (cons (car list) (delete-player (cdr list) player)))))
+
+; Obtiene una de las versiones del jugador de la poblacion del jugador del mismo tipo
+(define (get-version-player population)
+  (car population))
+
+; Realiza la seleccion de los jugadores con mayor fitness
+(define (selection population total-players)
+  (cond ((> total-players 0)
+         (cond ((> (get-fitness(car population)) 3)
+                 (cons (car population) (selection (cdr population)(- total-players 1))))
+               (else
+                (selection (cdr population)(- total-players 1)))))))
+
+; Cruce: realiza el cruce de las caracteristica entre dos jugadores
+(define (cruce player)
+  (list (get-id-team player)(get-id-player player)(/ (+ (get-movement player)(get-movement player)) 2) (/ (+ (get-force player)(get-force player)) 2)))
+
 ; Crea la poblacion sobre la cual va a actuar el genetico
 (define (create-population total-players id-team)
   (cond ((> total-players 0)
          (cons (fitness (create-player total-players id-team)) (create-population (- total-players 1) id-team)))))
-(create-population 10 1)
 
 ; Aplica seleccion y cruce en el algoritmo genetico
+(define (genetic total-generations)
+  (selection (create-population total-generations 1) total-generations))
 
-#|
-(game (create-team 1 1) (create-team 1 2))
+
+
+(genetic 100)
+
+;(create-population 10 1)
+;(game (create-team 1 1) (create-team 1 2))
 
 (get-team 2 (game (create-team 1 1) (create-team 1 2)))
 (get-player (get-team 1 (game (create-team 1 1) (create-team 1 2))) 6)
-(get-id-player (get-player (get-team 1 (game (create-team 1 1) (create-team 1 2))) 6))
-
 (get-id-team (get-player (get-team 1 (game (create-team 1 1) (create-team 1 2))) 6))
-
-(get-position (get-player (get-team 1 (game (create-team 1 1) (create-team 1 2))) 6))
-
+(get-id-player (get-player (get-team 1 (game (create-team 1 1) (create-team 1 2))) 6))
 (get-movement (get-player (get-team 1 (game (create-team 1 1) (create-team 1 2))) 6))
-
-(get-force (get-player (get-team 1 (game (create-team 1 1) (create-team 1 2))) 6))|#
+(get-force (get-player (get-team 1 (game (create-team 1 1) (create-team 1 2))) 6))
